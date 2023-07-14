81e1aeff3ecee916dcb01144650553bf8a5e470a
James Womack <james@womack.io>
2023-07-14 00:21:11 -0700

    docs(setup): note that I needed to config git identity

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 # git config --global init.defaultBranch main
 # git init
+# git config --global user.name "James Womack"
+# git config --global user.email james@womack.io
 nvm use
 corepack enable
 pnpm install
